f57661ddf22e5ff4790f0fec1949ba56c2e2a716
ma yingkui <maeikei@gmail.com>
2013-11-06 22:20:03 +0900

    change nginx start files.

--- start-service.sh
+++ start-service.sh
@@ -1,6 +1,18 @@
 #/bin/bash
-mkdir -p ./logs/
-touch ./logs/error.log
-/opt/nginx/sbin/nginx -p ./ -t -c www_conf/nginx.conf
-/opt/nginx/sbin/nginx -p ./ -c www_conf/nginx.conf
+
+mkdir -p /home/picuntu/workspace/sCamera/opt/nginx/logs
+touch /home/picuntu/workspace/sCamera/opt/nginx/logs/error.log
+
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera/opt/nginx -t -c www_conf/nginx.conf
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera/opt/nginx -c www_conf/nginx.conf
+
+
+
+
+
+mkdir -p /home/picuntu/workspace/sCamera/logs
+touch /home/picuntu/workspace/sCamera/logs/error.log
+
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera -t -c www_conf/nginx.conf
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera -c www_conf/nginx.conf
 
