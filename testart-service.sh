98dc42e417c42c2dc10c18d17a66bba6eab7cfba
Ma Yingkui <maeikei@gmail.com>
2013-11-06 19:45:44 +0530

    ssl configs

---
+++ testart-service.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+mkdir -p /home/picuntu/workspace/sCamera/opt/nginx/logs
+touch /home/picuntu/workspace/sCamera/opt/nginx/logs/error.log
+
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera/opt/nginx -t -c conf/nginx.conf
+
+
+
+
+
+mkdir -p /home/picuntu/workspace/sCamera/logs
+touch /home/picuntu/workspace/sCamera/logs/error.log
+
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera -t -c www_conf/nginx.conf
+
