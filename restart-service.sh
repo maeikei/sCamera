f769ce450d28f52301717fea52c001dd44211749
Ma Yingkui <maeikei@gmail.com>
2013-11-06 19:33:44 +0530

    start

--- restart-service.sh
+++ restart-service.sh
@@ -4,7 +4,7 @@ mkdir -p /home/picuntu/workspace/sCamera/opt/nginx/logs
 touch /home/picuntu/workspace/sCamera/opt/nginx/logs/error.log
 
 /opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera/opt/nginx -t -c conf/nginx.conf
-/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera/opt/nginx -c conf/nginx.conf
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera/opt/nginx -s reload -c conf/nginx.conf
 
 
 
@@ -14,5 +14,5 @@ mkdir -p /home/picuntu/workspace/sCamera/logs
 touch /home/picuntu/workspace/sCamera/logs/error.log
 
 /opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera -t -c www_conf/nginx.conf
-/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera -c www_conf/nginx.conf
+/opt/nginx/sbin/nginx -p /home/picuntu/workspace/sCamera -s reload -c www_conf/nginx.conf
 
