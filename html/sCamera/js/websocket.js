7dd244338296aeb09a952b79279bbb20f171cdf9
ma yingkui <maeikei@gmail.com>
2013-11-11 23:52:49 +0900

    add message server.

--- html/sCamera/js/websocket.js
+++ html/sCamera/js/websocket.js
@@ -1,4 +1,4 @@
-var messageServer = new WebSocket('wss://', ['soap', 'xmpp']);
+var messageServer = new WebSocket('wss://www.watorvapor.com/sCamera/messageServer.php', ['soap', 'xmpp']);
 // When the connection is open, send some data to the server
 messageServer.onopen = function () {
   messageServer.send('Ping'); // Send the message 'Ping' to the server
