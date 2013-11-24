2af061dc21c8649e37e6e9e52fabb957ef9b16f7
Ma Yingkui <maeikei@gmail.com>
2013-11-24 17:24:25 +0530

    change files

--- websocket/nodejs-version/server.js
+++ websocket/nodejs-version/server.js
@@ -1,20 +1,58 @@
-var http = require('https');
-var sockjs = require('sockjs');
-var fs = require("fs");
+(function(){
 
-var options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
-};
+    "use strict";
 
-var mCamera = sockjs.createServer();
-mCamera.on('connection', function(conn) {
-    conn.on('data', function(message) {
-        conn.write(message);
+    var fs = require('fs');
+
+    // you'll probably load configuration from config
+    var cfg = {
+        ssl: true,
+        port: 8453,
+        ssl_key: 'server.key',
+        ssl_cert: 'server.crt'
+    };
+
+    var httpServ = ( cfg.ssl ) ? require('https') : require('http');
+
+    var WebSocketServer   = require('ws').Server;
+
+    var app      = null;
+
+    // dummy request processing
+    var processRequest = function( req, res ) {
+
+        res.writeHead(200);
+        res.end("All glory to WebSockets!\n");
+    };
+
+    if ( cfg.ssl ) {
+
+        app = httpServ.createServer({
+
+            // providing server with  SSL key/cert
+            key: fs.readFileSync( cfg.ssl_key ),
+            cert: fs.readFileSync( cfg.ssl_cert )
+
+        }, processRequest ).listen( cfg.port );
+
+    } else {
+
+        app = httpServ.createServer( processRequest ).listen( cfg.port );
+    }
+
+    // passing or reference to web server so WS would knew port and SSL capabilities
+    var wss = new WebSocketServer( { server: app } );
+
+
+    wss.on( 'connection', function ( wsConnect ) {
+
+        wsConnect.on( 'message', function ( message ) {
+
+            console.log( message );
+
+        });
+
     });
-    conn.on('close', function() {});
-});
 
-var server = http.createServer(options);
-//mCamera.installHandlers(server, {prefix:'/echo'});
-server.listen(8453, '0.0.0.0');
+
+}());
