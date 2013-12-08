aa7ae0a913e9388d5394ad697aaf76c3d1d3684d
Ma Yingkui <maeikei@gmail.com>
2013-12-08 20:58:25 +0530

    websoket

--- html/mCamera/js/websocket.js
+++ html/mCamera/js/websocket.js
@@ -1,6 +1,9 @@
 var messageServer = new WebSocket('wss://' + location.hostname + '/mCameraWss');
 // When the connection is open, send some data to the server
 messageServer.onopen = function () {
+	var keyId = window.location.search.replace("?key=","");
+	console.log(keyId);
+	messageServer.send(keyId);
 };
 // Log errors
 messageServer.onerror = function (error) {
